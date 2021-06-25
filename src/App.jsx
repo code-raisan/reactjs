ed8cb6b8d2f5a5d4c583558c15e78f3befcb77dd
code-raisan <ytpc26ms@outlook.jp>
2021-06-26 00:16:32 +0900

    reactapp

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <h1>
+          Hello ReactJS
+          <span>
+            <img src={logo} alt="logo" className="title_logo" />
+          </span>
+        </h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
